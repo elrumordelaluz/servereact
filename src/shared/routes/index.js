0de981a6c6ffe7437597331e9acddbb9bf23d3be
Lionel T <elrumordelaluz@hotmail.com>
2016-04-28 15:48:07 +0200

    Remove unused Components

--- src/shared/routes/index.js
+++ src/shared/routes/index.js
@@ -3,13 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from '../components/App';
 import Index from '../components/Index';
-import Result from '../components/Result';
 import NotFound from '../components/NotFound';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Index}/>
-    <Route path="/search/:term" component={Result} />  
-    <Route path="*" component={NotFound} />  
+    <Route path="*" component={NotFound} />
   </Route>
 );
